ad0491793e0c05bce8b1ef5ec440acfa18a34965
Cormac Neeson <cormac.neeson@hmhco.com>
2021-06-07 13:05:55 +0100

    feat: add basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import CarList from 'Components/CarList/Carlist';
 
+
 const App = () => (
-  <>
-    <CarList />
-  </>
+  <Router>
+    <Switch>
+      <Route path="/car">
+        <div>Specific car</div>
+      </Route>
+      <Route path="/">
+        <CarList />
+      </Route>
+    </Switch>
+
+  </Router>
 );
 
 export default App;
