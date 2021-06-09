7f79d0c6bd5b5c4b2203c25d698bb5ef5e87fc1e
Cormac Neeson <cormac.neeson@hmhco.com>
2021-06-09 16:51:39 +0100

    feat: add pickup and return info

--- src/components/CarListItem/CarListItem.js
+++ src/components/CarListItem/CarListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as keys from 'Constants/car-keys.js';
 import { useHistory } from "react-router-dom";
-import { Card, Typography, Button, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+import { Card, CardMedia, Typography, Button, Grid, Paper } from '@material-ui/core';
 import { useStyles } from './CarListItem.styles';
 
 import InfoChip from 'Components/InfoChip';
@@ -15,7 +15,7 @@ const CarListItem = (props) => {
   const handleCarSelection = (carId) => history.push(`/car/${carId}`);
 
   return (
-    <Card className={classes.cardRoot}>
+    <Card className={classes.cardRoot} variant="outlined">
       <Grid
         container
         direction="column"
@@ -23,6 +23,7 @@ const CarListItem = (props) => {
       >
         <Typography variant="h5">{car[keys.NAME]}</Typography>
         <Typography variant="subtitle2">{car[keys.VENDOR_NAME]}</Typography>
+        <CardMedia component="img" className={classes.cardImage} image={car.PictureURL} />
         <Grid
           container
           direction="row"
@@ -34,9 +35,9 @@ const CarListItem = (props) => {
           <InfoChip title="Passengers" value={car[keys.PASSENGER_QUANTITY]} />
           <InfoChip title="Doors" value={car[keys.DOOR_COUNT]} />
         </Grid>
-        <Button variant="contained" onClick={() => handleCarSelection(car.uid)}>View car</Button>
+        <Button className={classes.viewCarButton} variant="contained" onClick={() => handleCarSelection(car.uid)}>View car</Button>
       </Grid>
-    </Card >
+    </Card>
   )
 }
 
--- src/components/SelectedCar/SelectedCar.js
+++ src/components/SelectedCar/SelectedCar.js
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import { getCarById } from 'API/cars.js';
+import CarListItem from 'Components/CarListItem'
 
 const SelectedCar = () => {
 
   let { carId } = useParams();
+  const [car, setCar] = useState(null);
 
   useEffect(() => {
-    console.log(getCarById(carId));
+    //console.log(getCarById(carId));  //DEBUG
+    const selectedCar = getCarById(carId);
+    setCar(selectedCar);
   }, []);
 
   return (
-    <div></div>
+    car ? <CarListItem car={car} /> : null
   )
 }
 
